b26a6df9f5e8331163ea32f03362a90409c8a3be
Stephanie Noe <144886264+stephkmn@users.noreply.github.com>
2026-02-24 15:00:29 -0800

    Add project folder structure and App.js

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Welcome to E2EE Chat App</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
