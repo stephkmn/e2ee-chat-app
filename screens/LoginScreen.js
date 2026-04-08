7f8c8cc68eb5343c8155feb36f57692828d91f5b
jordan flores <jordanflores@Mac.lan>
2026-04-07 19:57:57 -0700

    fix: update SafeAreaView import per PR review

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, SafeAreaView } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function LoginScreen() {
   // These state variables temporarily hold the text the user types into the inputs
--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, SafeAreaView } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function RegisterScreen() {
   const [name, setName] = useState('');
