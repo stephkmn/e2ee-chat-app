d82442fb684cc1e15518c16091e6021e9101322f
Stephanie Noe <144886264+stephkmn@users.noreply.github.com>
2026-04-11 14:44:06 -0700

    Remove log in success alert

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -46,7 +46,6 @@ export default function LoginScreen({ navigation }) {
             if (error) {
               Alert.alert("Login Failed", error);
             } else {
-              Alert.alert("Success!", "You are logged in.");
               navigation.navigate('Chats');
             }
           }}
